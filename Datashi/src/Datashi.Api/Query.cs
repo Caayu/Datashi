3bb35010bdcbb816d571a8cabc80c2c01446af5f
Caayu <henrycay@outlook.com>
2024-10-01 22:52:05 -0300

    Add project files.

---
+++ Datashi/src/Datashi.Api/Query.cs
@@ -0,0 +1,11 @@
+﻿namespace Datashi.Api;
+
+public class Query
+{
+    public Sushi GetSushi() =>
+        new()
+        {
+            Name = "Sushi",
+            Description = "Taste sushi"
+        };
+}
---
+++ Datashi/src/Datashi.Api/Sushi.cs
@@ -0,0 +1,7 @@
+﻿namespace Datashi.Api;
+
+public class Sushi
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
